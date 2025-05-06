a164d6cfd6b9a20bdf3b544d23e6a5efbb7e8d1f
Aidan Milholland <amilholland22@gmail.com>
2025-05-06 02:29:07 -0400

    maybe

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import type { Schema } from "../amplify/data/resource"; // Import the Schema type
-import { generateClient } from "aws-amplify/data"; // Import the function to generate a client
-import { useAuthenticator } from "@aws-amplify/ui-react"; // Import the Authenticator hook
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
-// Generate the client using the Schema type
 const client = generateClient<Schema>();
 
 function App() {
-  // Destructure user and signOut from the useAuthenticator hook
   const { user, signOut } = useAuthenticator();
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  // Extract the username from the user's loginId or default to "User"
-  const displayedUsername = user?.signInDetails?.loginId?.split('@')[0] || "User";
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
 
+  
+
+
+             // "jane.doe"
+             const username = user?.signInDetails?.loginId?.split('@')[0] || "User";
   return (
     <main>
-      {/* Display a welcome message with the username */}
-      <h1>Thank you {displayedUsername} for signing in.</h1>
-
-      {/* Button to sign out */}
+      
+      <h1>Thank you {username} for signing in</h1>
+        
+      
       <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
 
-export default App; // Export the App component
+export default App;
